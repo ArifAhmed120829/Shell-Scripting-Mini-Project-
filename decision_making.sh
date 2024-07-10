81496f829b4d2c7f7c3cda1d83e6968fe9ab6dd4
ArifAhmed <arif15-12301@diu.edu.bd>
2024-07-10 06:01:22 +0600

    wip

---
+++ decision_making.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "Input three numbers"
+read a
+read b
+read c
+
+if [ $a == $b -a $b == $c -a $a == $c ]
+then
+echo EQUILATERAL
+
+elif [ $a == $b -o $b == $c -o $a == $c ]
+then
+echo ISOSCELES
+else
+echo SCALAENE
+fi
---
+++ loop.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+for i in {1..5}
+do
+echo $i
+done
+
+for x in Arif is a student
+do
+echo $x
+done
+
+j=1
+while [[ $j -le 10 ]] ; do
+echo "$j"
+(( j += 1 ))
+done
+
